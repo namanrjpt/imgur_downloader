efa2889ab14614a100df2117f8b2ce9d1b04beed
Naman Rajput <mail.namanrajput@gmail.com>
2016-12-23 21:50:11 +0530

    Adding the script

---
+++ imgur_downloader.ps1
@@ -0,0 +1,14 @@
+$imgurURL = Read-Host -Prompt "Enter the URL of the IMGUR album: "
+$page = Invoke-WebRequest -Uri $imgurURL -UseBasicParsing
+if((Test-Path ".\Tuna") -eq $False){
+    mkdir ".\Images"
+}
+foreach($image in $page.Images){
+    if($image.itemprop -eq "contentURL"){
+	    echo ([string]"Downloading image " + [string]$page.Images.IndexOf($image))
+	    $image_url = $image.src.replace("//", "http://")
+	    $image_name = $image.src.Substring(14)
+	    Invoke-WebRequest -Uri $image_url -OutFile ".\Images\$image_name" -UseBasicParsing
+
+    }
+}
